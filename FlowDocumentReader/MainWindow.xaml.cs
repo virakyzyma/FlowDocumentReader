0c47e3b6a2aac9bb9286fa31d72b1a33554d7496
Vira Kyzyma <vera.kyzyma8@gmail.com>
2024-07-22 19:14:35 +0300

    Add project files.

---
+++ FlowDocumentReader/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlowDocumentReader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(ProductTextBox.Text))
+            {
+                MessageBox.Show("Empty string", "Invalid data");
+                return;
+            }
+
+            productListBox.ListItems.Add(new ListItem(new Paragraph(new Run(ProductTextBox.Text))));
+            ProductTextBox.Clear();
+        }
+    }
+}
